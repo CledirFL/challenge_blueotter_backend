5aa4cef02843478bba70f8c315dc130a7606f38b
Kelton <keltonfernandes@live.com>
2024-07-20 10:35:06 -0100

    feat: config conextion with env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { GithubModule } from './github/github.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'github',
-      entities: [],
-      synchronize: true, //do not use in prod
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: [],
+        synchronize: process.env.DB_SYNCHRONIZE === 'true' ? true : false,
+      })
     }),
+    GithubModule,
   ],
   controllers: [AppController],
   providers: [AppService],
