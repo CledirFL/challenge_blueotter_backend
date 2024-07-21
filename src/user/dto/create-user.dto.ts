92b799a51d38b60a7e194fb2219101263e7529da
Kelton <keltonfernandes@live.com>
2024-07-21 22:08:23 -0100

    feat: userService to create user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,60 @@
-export class CreateUserDto {}
+import { IsString, IsInt, IsBoolean, IsOptional, IsUrl } from 'class-validator';
+
+export class CreateUserDto {
+    @IsInt()
+    id: number;
+  
+    @IsString()
+    login: string;
+  
+    @IsOptional()
+    @IsString()
+    node_id?: string;
+  
+    @IsOptional()
+    @IsUrl()
+    avatar_url?: string;
+  
+    @IsOptional()
+    @IsString()
+    gravatar_id?: string;
+  
+    @IsUrl()
+    url: string;
+  
+    @IsUrl()
+    html_url: string;
+  
+    @IsUrl()
+    followers_url: string;
+  
+    @IsUrl()
+    following_url: string;
+  
+    @IsUrl()
+    gists_url: string;
+  
+    @IsUrl()
+    starred_url: string;
+  
+    @IsUrl()
+    subscriptions_url: string;
+  
+    @IsUrl()
+    organizations_url: string;
+  
+    @IsUrl()
+    repos_url: string;
+  
+    @IsUrl()
+    events_url: string;
+  
+    @IsUrl()
+    received_events_url: string;
+  
+    @IsString()
+    type: string;
+  
+    @IsBoolean()
+    site_admin: boolean;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+  private userRepository: Repository<User>) { }
+
+  private readonly logger: Logger = new Logger(User.name);
+
+
+  async create(createUserDto: CreateUserDto) {
+    this.logger.log('CREATE: Creating user')
+    try {
+      this.logger.log('CREATE: Checking if user already exists');
+      const foundUser = await this.userRepository.findOne({
+        where: {
+          login: createUserDto.login,
+          id: createUserDto.id,
+        }
+        
+      })
+      if (foundUser) {
+        this.logger.log('CREATE: User found');
+        return foundUser;
+      }
+
+      this.logger.log('CREATE: Creating new User');
+      const newUser = this.userRepository.create();
+      const savedUser = await this.userRepository.save({
+        ...newUser,
+        ...createUserDto,
+      });
+      if (savedUser) {
+        this.logger.log('CREATE: User created successfully');
+      }
+
+      return savedUser;
+      
+    } catch (error) {
+      this.logger.error(`CREATE: Error ${error.message}`);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  async findOneByLogin(){
+    
   }
 
   findAll() {
