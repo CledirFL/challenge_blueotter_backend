de90b046639fed68e5c3cb2547640525102521d3
Kelton <keltonfernandes@live.com>
2024-07-22 08:55:06 -0100

    refactor: change text in app services to be display in "/"

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `O desafio consiste em desenvolver uma solução que permita interagir e manipular dados de utilizadores do GitHub.`;
   }
 }
