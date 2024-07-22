de046e72df2736dd6c2d180283cbccdc55f72aba
Kelton <keltonfernandes@live.com>
2024-07-22 08:14:45 -0100

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blueotter challenge')
+    .setDescription('The challenge API description')
+    .setVersion('1.0')
+    .addTag('blueotter')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
